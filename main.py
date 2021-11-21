d51b576be9feb308f215c668b4451d1b24c02136
G.A.JAGUAR <dev.gajaguar@gmail.com>
2021-11-21 15:32:03 +0000

    :sparkles: chore(endpoints): add `detail` with query parameters validation

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel
 # FastAPI
 from fastapi import FastAPI
 from fastapi import Body
+from fastapi import Query
 
 app = FastAPI()
 
@@ -26,3 +27,20 @@ def home():
 def create_person(person: Person = Body(...)):
     return person
 
+@app.get('/person/detail')
+def show_person(
+    name: Optional[str] = Query(
+        None,
+        min_length=1,
+        max_length=50,
+        name='name',
+        description='The name of the person. It must be between 1 and 50',
+        ),
+    age: int = Query(
+        ...,
+        ge=18,
+        name='Age',
+        description='The age of the person. Only accepts over 18 years old'
+        )
+):
+    return {name: age}
